74be252221fbfe719fa31863c13451ab71497a0f
Renan <renanmiguel93@hotmail.com>
2018-10-17 18:18:59 +0000

    Classe abstrata View e método abstrato template criados

---
+++ app/ts/views/View.ts
@@ -0,0 +1,14 @@
+abstract class View<T> {
+
+    private _elemento: Element;
+
+    constructor(seletor: string) {
+        this._elemento = document.querySelector(seletor);
+    }
+
+    update(model: T): void {
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    abstract template(model: T): string;
+}
