da4977e89762ad1df74371b96896f5e2d5618345
Renan <renanmiguel93@hotmail.com>
2018-11-05 12:39:02 +0000

    Trocando private para readonly nas propriedades da classe Negociacao

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,21 +1,9 @@
 export class Negociacao {
 
-    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
-
-    get data() {
-        return this._data;
-    }
-    
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
+    constructor(readonly data: Date, readonly quantidade: number, readonly valor: number) {}
 
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 
 }
